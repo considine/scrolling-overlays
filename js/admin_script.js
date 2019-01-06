f2957da03d4c45262e33d32221eb91d42d61488a
John Considine <jackconsidine3@gmail.com>
2019-01-05 19:36:37 -0600

    feat: add support for youtube embeds

---
+++ js/admin_script.js
@@ -0,0 +1,169 @@
+(function() {
+  class MediaFetcher {
+    constructor(onMediaCb, type = 'video') {
+      this.onMediaCb = onMediaCb;
+      this.image_frame = wp.media({
+        title: 'Select Media',
+        multiple: false,
+        library: {
+          type
+        }
+      });
+
+      this.image_frame.on('close', this.onFrameClose.bind(this));
+      this.image_frame.on('open', this.onFrameOpen.bind(this));
+    }
+
+    open() {
+      this.image_frame.open();
+    }
+
+    onFrameOpen() {}
+
+    onFrameClose() {
+      var selection = this.image_frame.state().get('selection');
+      const urls = [];
+      var my_index = 0;
+      selection.each(function(attachment) {
+        urls[my_index] = {
+          url: attachment.attributes.url,
+          filename: attachment.attributes.filename
+        };
+
+        my_index++;
+      });
+
+      if (this.onMediaCb) {
+        this.onMediaCb(urls);
+      }
+    }
+  }
+
+  function OnOverlayGenerated(shortcode) {
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+    const c = document.createElement('code');
+    p.appendChild(
+      document.createTextNode('Shortcode generated! Copy this into your post:')
+    );
+    c.appendChild(document.createTextNode(shortcode));
+
+    div.appendChild(p);
+    div.appendChild(c);
+    document.getElementById('shortcodeResp').appendChild(div);
+  }
+
+  jQuery(document).ready(function($) {
+    const videoFetcher = new MediaFetcher(function(urls) {
+      const data = {
+        action: 'koptional_create_overlay',
+        url: urls[0].url,
+        name: urls[0].filename,
+        type: 'VIDEO'
+      };
+      jQuery.post(ajaxurl, data, function(resp) {
+        const response = JSON.parse(resp);
+        if (response.success && response.shortcode) {
+          OnOverlayGenerated(response.shortcode);
+        }
+      });
+    }, 'video');
+
+    const imageFetcher = new MediaFetcher(function(urls) {
+      const data = {
+        action: 'koptional_create_overlay',
+        url: urls[0].url,
+        name: urls[0].filename,
+        type: 'IMAGE'
+      };
+      jQuery.post(ajaxurl, data, function(resp) {
+        const response = JSON.parse(resp);
+        if (response.success && response.shortcode) {
+          OnOverlayGenerated(response.shortcode);
+        }
+      });
+    }, 'image');
+
+    jQuery('input#image_media_manager').click(function(e) {
+      e.preventDefault();
+      imageFetcher.open();
+    });
+    jQuery('input#video_media_manager').click(function(e) {
+      e.preventDefault();
+      videoFetcher.open();
+    });
+
+    jQuery('form#youtubeForm').submit(function(e) {
+      e.preventDefault();
+      const url = $('input#youtube_url').val();
+      $('input#youtube_url').val('');
+      const data = {
+        action: 'koptional_create_youtube_overlay',
+        url
+      };
+      jQuery
+        .post(ajaxurl, data, function(resp_raw) {
+          const response = JSON.parse(resp_raw);
+          if (response.success && response.shortcode) {
+            OnOverlayGenerated(response.shortcode);
+          }
+        })
+        .fail(function(response) {
+          const err = JSON.parse(response.responseText);
+          alert(err.data);
+        });
+    });
+
+    // Fetch existing
+    jQuery.post(
+      ajaxurl,
+      {
+        action: 'koptional_get_overlays'
+      },
+      function(resp_raw) {
+        const resp = JSON.parse(resp_raw);
+        const table = document.createElement('table');
+
+        const header = document.createElement('thead');
+        const headRow = document.createElement('tr');
+        const shortcodeHeader = document.createElement('td');
+        const nameHeader = document.createElement('td');
+        const typeHeader = document.createElement('td');
+
+        shortcodeHeader.appendChild(document.createTextNode('Shortcode'));
+        typeHeader.appendChild(document.createTextNode('Media Type'));
+        nameHeader.appendChild(document.createTextNode('File Name'));
+
+        headRow.appendChild(shortcodeHeader);
+        headRow.appendChild(typeHeader);
+        headRow.appendChild(nameHeader);
+
+        header.appendChild(headRow);
+
+        table.appendChild(header);
+
+        _.each(resp.data, item => {
+          const row = document.createElement('tr');
+          const shortcode = document.createElement('td');
+          shortcode.appendChild(document.createTextNode(item.shortcode));
+          const fname = document.createElement('td');
+          const link = document.createElement('a');
+          link.setAttribute('href', item.url);
+          link.setAttribute('target', '_blank');
+          link.appendChild(document.createTextNode(item.name));
+          fname.appendChild(link);
+
+          const ftype = document.createElement('td');
+          ftype.appendChild(document.createTextNode(item.type));
+
+          row.appendChild(shortcode);
+          row.appendChild(ftype);
+          row.appendChild(fname);
+          table.appendChild(row);
+        });
+
+        document.getElementById('pastOverlays').appendChild(table);
+      }
+    );
+  });
+})();
--- koptional-video-overlay.php
+++ koptional-video-overlay.php
@@ -7,8 +7,8 @@ Version: 0.1.0
 Author: Jack Considine
  */
 
-function wporg_options_page()
-{
+define('KOPTIONAL_OVERLAY_TABLE', 'koptional_overlays');
+function wporg_options_page() {
     // add top level menu page
     add_menu_page(
         'Video Overlay',
@@ -21,71 +21,169 @@ function wporg_options_page()
 add_action('admin_menu', 'wporg_options_page');
 
 // https://wordpress.stackexchange.com/questions/235406/how-do-i-select-an-image-from-media-library-in-my-plugin
-function koptional_video_overlays()
-{
+function koptional_video_overlays() {
     $image_id = get_option('myprefix_image_id');
     ?>
-<img style="max-width: 100%; width: 500px;" src="<?php echo plugins_url('/embed.gif', __FILE__) ?>">
-<p class="noselect"> <small> For videos, you can switch to Youtube. after generating your shortcode the normal way
-add a youtube attribute (and optionally a fallback URL where the User can click a link). It will look like this:
-</small> </p>
-<img style="max-width: 100%; width: 500px;"   alt="Instructions" src="<?php echo plugins_url('/demoimage.png', __FILE__) ?>">
-
+<h2>
+    <?php esc_attr_e('For self hosted media');?>
+</h2>
 <p id="shortcodeResp"> </p>
 <input type='button' class="button-primary" value="<?php esc_attr_e('Select an image');?>" id="image_media_manager" />
 <input type='button' class="button-primary" value="<?php esc_attr_e('Select a video');?>" id="video_media_manager" />
+
+<h2>
+    <?php esc_attr_e('For Youtube videos');?>
+</h2>
+
+<form id="youtubeForm" class="koptional-style" style="padding-bottom: 16px;">
+    <label>Youtube URL <br> <input type="text" required="required" id="youtube_url">
+    </label>
+    <br>
+    <br>
+    <input type='submit' class="button-primary" value="<?php esc_attr_e('Select a video');?>" id="youtube_media_manager" />
+</form>
+
+<div id="overlaySchema" class="koptional-style">
+    <h2> Shortcode Attributes </h2>
+    <table>
+        <thead>
+            <tr>
+                <td> </td>
+                <td> Video </td>
+                <td> Image </td>
+                <td> Youtube Video </td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">id </th>
+                <td> Required </td>
+                <td> Required </td>
+                <td> Required </td>
+            </tr>
+            <tr>
+                <th scope="row">fallbackurl </th>
+                <td> x </td>
+                <td> x </td>
+                <td> Optional- this link will display if the youtube video doesn't load </td>
+            </tr>
+            <tr>
+                <th scope="row">caption </th>
+                <td> x </td>
+                <td> Optional- caption that appears above the image </td>
+                <td> x </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<hr>
+<div id="pastOverlays" class="koptional-style">
+    <h2> Created Shortcodes </h2>
+</div>
+
 <?php
 }
 
 // As you are dealing with plugin settings,
 // I assume you are in admin side
 add_action('admin_enqueue_scripts', 'load_wp_media_files');
-function load_wp_media_files($page)
-{
+function load_wp_media_files($page) {
     // change to the $page where you want to enqueue the script
     if ($page == 'toplevel_page_wporg') {
         // Enqueue WordPress media scripts
         wp_enqueue_media();
         // Enqueue custom script that will interact with wp.media
-        wp_enqueue_script('myprefix_script', plugins_url('/js/myscript.js', __FILE__), array('jquery'), '0.1');
+        wp_enqueue_script('koptional_admin_script', plugins_url('/js/admin_script.js', __FILE__), array('jquery'), '0.1');
+        wp_enqueue_script('koptional_plugin_script', plugins_url('/static/main.81eda2fc.js', __FILE__), array(), '0.1');
     }
 }
 
-add_action('wp_ajax_add_review', 'koptional_add_video_overlay');
-function koptional_add_video_overlay()
-{
+add_action('wp_ajax_koptional_create_overlay', 'koptional_add_video_overlay');
+add_action('wp_ajax_koptional_create_youtube_overlay', 'koptional_add_youtube_overlay');
+function koptional_add_video_overlay() {
     global $wpdb;
-
-    $table_name = $wpdb->prefix . 'koptional_overlay';
-    if (isset($_POST['url']) && isset($_POST["type"])) {
+    $table_name = $wpdb->prefix . KOPTIONAL_OVERLAY_TABLE;
+    if (isset($_POST['url']) && isset($_POST["type"]) && isset($_POST['name'])) {
         $type = $_POST['type'];
         $t = $wpdb->insert(
             $table_name,
             array(
                 "url" => "{$_POST['url']}",
                 "type" => "{$type}",
+                'fname' => "{$_POST['name']}",
             ));
         $insert_id = $wpdb->insert_id;
 
-        if ($type == "VIDEO") {
-            $shortcode = "[kopoverlay id=\"{$insert_id}\" type=\"video\"]";
-        } else if ($type == "IMAGE") {
-            $shortcode = "[kopoverlay id=\"{$insert_id}\" type=\"image\" caption=\"\"]";
-        }
+        $shortcode = koptional_generate_shortcode($insert_id, $type);
         echo json_encode(["success" => $t, "shortcode" => $shortcode]);
     }
     wp_die();
 }
 
-global $kop_db_version;
-$kop_db_version = '1.0';
+function koptional_add_youtube_overlay() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . KOPTIONAL_OVERLAY_TABLE;
+    if (isset($_POST['url'])) {
+        try {
+        $embed = koptional_get_youtube_embed($_POST['url']);
 
-function kop_install()
-{
+            $t = $wpdb->insert(
+            $table_name,
+            array(
+                "url" => "{$embed}",
+                "type" => "YOUTUBE",
+                'fname' => "{$embed}",
+            ));
+            $insert_id = $wpdb->insert_id;
+
+            $shortcode = koptional_generate_shortcode($insert_id, 'YOUTUBE');
+            echo json_encode(["success" => $t, "shortcode" => $shortcode]);
+            wp_die();
+
+        } catch (Exception $e) {
+            // Return error
+            return wp_send_json_error("Invalid Youtube URL", 400);
+        }
+    }
+
+}
+
+function koptional_generate_shortcode($id, $type) {
+    if ($type == "VIDEO") {
+        return "[kopoverlay id=\"{$id}\"]";
+    } else if ($type == "IMAGE") {
+        return "[kopoverlay id=\"{$id}\" caption=\"\"]";
+    } else if ($type === "YOUTUBE") {
+        return "[kopoverlay id=\"{$id}\" fallbackurl=\"\"]";
+    }
+}
+
+add_action("wp_ajax_koptional_get_overlays", 'koptional_get_video_overlays');
+function koptional_get_video_overlays() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . KOPTIONAL_OVERLAY_TABLE;
+    $query = $wpdb->get_results('SELECT * FROM ' . $table_name . ' ORDER BY id DESC');
+    $results = [];
+    foreach ($query as $row) {
+        array_push($results, [
+            "shortcode" => koptional_generate_shortcode($row->id, $row->type),
+            "name" => $row->fname,
+            "url" => $row->url,
+            "type" => strtolower($row->type)
+        ]);
+    }
+    echo json_encode(["data" => $results]);
+    wp_die();
+}
+
+global $kop_db_version;
+$kop_db_version = '1.1';
+
+function kop_install() {
     global $wpdb;
     global $kop_db_version;
 
-    $table_name = $wpdb->prefix . 'koptional_overlay';
+    $table_name = $wpdb->prefix . KOPTIONAL_OVERLAY_TABLE;
 
     $charset_collate = $wpdb->get_charset_collate();
 
@@ -93,6 +191,7 @@ function kop_install()
 		id mediumint(9) NOT NULL AUTO_INCREMENT,
 		url varchar(200) DEFAULT '' NOT NULL,
         type varchar(15) DEFAULT 'VIDEO' NOT NULL,
+        fname varchar(50) NOT NULL,
 		PRIMARY KEY  (id)
 	) $charset_collate;";
 
@@ -104,8 +203,7 @@ function kop_install()
 
 register_activation_hook(__FILE__, 'kop_install');
 
-function kopoverlay_func($atts)
-{
+function kopoverlay_func($atts) {
     global $wpdb;
 
     $a = shortcode_atts(array(
@@ -117,22 +215,26 @@ function kopoverlay_func($atts)
         'fallbackurl' => null,
     ), $atts);
 
-    $table_name = $wpdb->prefix . 'koptional_overlay';
+    $table_name = $wpdb->prefix . KOPTIONAL_OVERLAY_TABLE;
     // return  'SELECT * FROM ' . $table_name . ' WHERE ID=' . "{$a['id']}";
     $ar = $wpdb->get_results('SELECT * FROM ' . $table_name . ' WHERE ID=' . "{$a['id']}");
     if (sizeof($ar) == 0) {
         return "";
     }
-
     $url = $ar[0]->url;
     $id = $ar[0]->id;
     $type = $ar[0]->type;
+    $shortcode = $ar[0];
+    if (!isset($url) || !isset($type)) {
+        return "";
+    }
     $caption = $a['caption'];
-    $youtube = $a["youtube"];
     $fallbackurl = $a["fallbackurl"];
     $style = $a['side'] == "right" ? "right: 0;" : "left: 0;";
-    if ($type == 'VIDEO' && $youtube) {
-        $fallbackHTML = $fallbackurl ? "<p class='fallback-text'> Trouble watching YouTube? Click <a target='_blank' href='{$fallbackurl}'>
+
+
+    if ($type == 'YOUTUBE') {
+        $fallbackHTML = ($fallbackurl && sizeof($fallbackurl) > 0) ? "<p class='fallback-text'> Trouble watching YouTube? Click <a target='_blank' href='{$fallbackurl}'>
         here </a> to watch instead </p>" : "";
         return <<<HTML
  <div data-type="koptional-youtube-overlay" data-target="{$id}" id="koptional-overlay-{$id}" class="koptional-overlay-insert">
@@ -140,9 +242,9 @@ function kopoverlay_func($atts)
         <!-- <video id="vid1" autoplay='autoplay' muted='muted' class="video-js vjs-default-skin" controls muted="muted"
             autoplay width="700" data-setup='{ "techOrder": ["youtube"], "sources": [{ "type": "video/youtube", "src": "https://www.youtube.com/watch?v=emNgfuw8vlA"}], "youtube": { "iv_load_policy": 1 } }'>
           </video> -->
-        <div class="koptional-embed-wrapper" style="max-width: 135vh;">
+        <div class="koptional-embed-wrapper" style="max-width: 130vh;">
             <div class="responsive-embed">
-                <iframe enablejsapi="1" id="youtube-video-{$id}" width="420" height="315" src="{$youtube}?enablejsapi=1&mute=1"
+                <iframe enablejsapi="1" id="youtube-video-{$id}" width="420" height="315" src="{$url}?enablejsapi=1&mute=1"
                 frameborder="0" allowfullscreen></iframe>
             </div>
         </div>
@@ -186,17 +288,31 @@ HTML;
 add_shortcode('kopoverlay', 'kopoverlay_func');
 
 // Enqueue static scripts and style
-function koptional_overlay_enqueue_style()
-{
+function koptional_overlay_enqueue_style() {
     wp_enqueue_style('video-js-css', plugins_url('/static/video-js.min.css', __FILE__), false);
     // wp_enqueue_style('koptional-montage-css', plugins_url('/static/montage.css', __FILE__), false);
     // wp_enqueue_style('koptional-video-css', plugins_url('/static/style.css', __FILE__), false);
 }
 
-function koptional_overlay_enqueue_script()
-{
-    wp_enqueue_script('koptional-js', plugins_url('/static/main.80eda2fc.js', __FILE__), false);
+function koptional_overlay_enqueue_script() {
+    wp_enqueue_script('koptional-js', plugins_url('/static/main.82eda2fc.js', __FILE__), false);
 }
 
 add_action('wp_enqueue_scripts', 'koptional_overlay_enqueue_style');
 add_action('wp_enqueue_scripts', 'koptional_overlay_enqueue_script');
+
+function koptional_get_youtube_embed($link) {
+    // Is video already in embedded form?
+    $regExp = '/.*\/embed.*\//';
+    preg_match($regExp, $link, $matches, PREG_OFFSET_CAPTURE);
+    if (sizeof($matches) === 1 && $matches[0][1] === 0) {
+        return $link;
+    }
+    $regExp = '/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/';
+    preg_match($regExp, $link, $matches, PREG_OFFSET_CAPTURE);
+    if (sizeof($matches) === 3) {
+        return "https://www.youtube.com/embed/" . $matches[2][0];
+    } else {
+        throw new Exception('Invalid Link');
+    }
+}
